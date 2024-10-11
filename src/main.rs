6a0e62137b6a045e516fabe8c8880fc0d05e21c8
Armaan Gupta <ohayouarmaan@gmail.com>
2024-10-11 22:07:42 +0530

    feat: responding to ping command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::prelude::*;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,8 +9,13 @@ fn main() {
    
      for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut valid_stream) => {
                 println!("accepted new connection");
+                let mut data: Vec<u8> = Vec::new();
+                let data_read = valid_stream.read(&mut data);
+                if let Ok(read) = data_read {
+                    valid_stream.write("+PONG\r\n".as_bytes());
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
